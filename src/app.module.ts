434d018b09593a7da64aaef42d5397f5a9500f18
Abena Amo <abenaserwaaamo@gmail.com>
2025-03-21 22:47:53 +0000

    modify read me and pushing apps.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import {CommsModule} from './comms/comms.module'
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [CommsModule],
   controllers: [AppController],
   providers: [AppService],
 })
